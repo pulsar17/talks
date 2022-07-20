87586d75f01f5ca77c95e646b5900a6f6f726630
Ishaan Arora <42387090+pulsar17@users.noreply.github.com>
2022-07-20 12:59:52 +0530

    add license to teach.nvim

--- pydelhi/teach.lua
+++ pydelhi/teach.lua
@@ -1,7 +1,26 @@
 -- teach.nvim - a poor man's companion shell-like-program that can be used as a teaching tool
+--
+-- Copyright 2022, Ishaan Arora (ishaanarora1000@gmail.com)
+--
+-- This file is part of the software teach.nvim.
+--
+-- teach.nvim is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- teach.nvim is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with teach.nvim.  If not, see <http://www.gnu.org/licenses/>.
+--
+
 
 local M = {}
-local activeWindows = {}
+M.activeWindows = {}
 
 -- Split input text by newlines akin to Python's splitlines()
 local splitlines = function(text) 
